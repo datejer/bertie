514c9f81dc3132cf7bf82746e1a95f666c0b914b
datejer <ejer.priv@wp.pl>
2021-02-25 14:06:15 +0100

    Change ellipse function to blob image + add some comments

--- sketch.js
+++ sketch.js
@@ -16,35 +16,49 @@ let timedOut = false;
 let bertieCenterX;
 let bertieCenterY;
 
+let blob;
+
+// Load all assets (fonts, images and audio)
 function preload() {
 	font = loadFont("assets/PatrickHand.ttf");
 
 	bertie = loadImage("assets/bertie.png");
 	congratulations = loadImage("assets/congratulations.png");
 
+	blob = loadImage("assets/blob.png");
+
 	soundFormats("wav", "mp3");
 	victorySound = loadSound("assets/victory.wav");
 	atmoSound = loadSound("assets/atmo.mp3");
 }
 
+// Setup canvas and bertie
 function setup() {
+	// Display block in order to prevent scrollbar
 	const canvas = createCanvas(windowWidth, windowHeight);
 	canvas.style("display", "block");
 
+	// Resize all image beforehand
 	bertie.resize(100, 0);
 	congratulations.resize(0, windowHeight);
 
+	blob.resize(150, 0);
+
+	// Pick a random position for Bertie to spawn in
 	x = random(0, windowWidth - bertie.width);
 	y = random(0, windowHeight - bertie.height);
 }
 
+// Keep canvas fullscreen even when resizing window
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 }
 
 function mousePressed() {
+	// Game start
 	if (!playing) playing = true;
 	else {
+		// Game reset (try again)
 		if (tryAgain) {
 			playing = true;
 			victory = false;
@@ -60,6 +74,7 @@ function mousePressed() {
 		}
 	}
 
+	// Alternatively you can click on bertie to win instead of waiting with the cursor in the perimeter.
 	if (
 		mouseX - bertieCenterX > -75 &&
 		mouseX - bertieCenterX < 75 &&
@@ -71,6 +86,7 @@ function mousePressed() {
 
 function draw() {
 	if (!playing) {
+		// Starting screen, appears only once
 		background(256);
 
 		textFont(font);
@@ -79,15 +95,22 @@ function draw() {
 		text("Click anywhere to start.", windowWidth / 2, windowHeight / 2);
 	} else {
 		if (!victory) {
+			// Main game loop
 			background(0);
 
+			// Ambient atmosphere sounds for extra emotions ;)
 			if (!atmoSoundPlayed) atmoSound.loop();
 			atmoSoundPlayed = true;
 
-			ellipse(mouseX, mouseY, 150, 150);
+			// Draw flashlight and bertie
+			// ellipse(mouseX, mouseY, 150, 150);
+			imageMode(CENTER);
+			image(blob, mouseX, mouseY);
 
+			imageMode(CORNER);
 			image(bertie, x, y);
 
+			// Calculate if cursor is in perimeter and add score to win
 			bertieCenterX = x + bertie.width / 2;
 			bertieCenterY = y + bertie.height / 2;
 
@@ -102,17 +125,20 @@ function draw() {
 			if (score > 50) victory = true;
 		} else {
 			if (!tryAgain) {
+				// Victory screen
 				background(256);
 
 				imageMode(CENTER);
 				image(congratulations, windowWidth / 2, windowHeight / 2);
 
+				// Pause atmo sound because you won \o/
 				if (!atmoSoundPaused) atmoSound.pause();
 				atmoSoundPaused = true;
 
 				if (!victorySoundPlayed) victorySound.play();
 				victorySoundPlayed = true;
 
+				// Try again screen appears after 4 seconds
 				if (!timedOut) {
 					setTimeout(() => {
 						tryAgain = true;
@@ -120,8 +146,10 @@ function draw() {
 				}
 				timedOut = true;
 			} else {
+				// Try again screen
 				background(256);
 
+				// Resume the atmo sound, back in the ambience soup
 				if (atmoSoundPlayed) atmoSound.loop();
 				atmoSoundPlayed = false;
 
